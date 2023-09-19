69d251b3a59e36793caa0c1deda1a08672ed7ebc
jhoward39 <70383367+jhoward39@users.noreply.github.com>
2023-09-19 14:49:11 -0400

    succesful build?

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import requests
 import flask
+import pandas
+import yfinance as yf
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
    print('Request for index page received')
-   return flask.render_template('index.html')
+   stock = get_ticker('AAPL')
+   return flask.render_template('index.html', name = 'AAPL', data = stock.to_html())
+
+def get_ticker(ticker):
+   ticker = [ticker]
+   # start = datetime.datetime(2020, 1, 1)
+   # end = datetime.datetime(2022, 1, 1)
+   data = yf.download(ticker)
+   return data
 
 
 if __name__ == '__main__':
