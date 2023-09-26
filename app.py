dfb7f029b3cf7066867c27cec416befc79247f0c
jhoward39 <70383367+jhoward39@users.noreply.github.com>
2023-09-26 14:58:32 -0400

    good headway

--- app.py
+++ app.py
@@ -1,15 +1,65 @@
 import flask
-import yfinance as yf
+import psycopg2
+from psycopg2 import extras
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
    print('Request for index page received')
+
    return flask.render_template('index.html', 
-            stocks_in_danger = ['AAPL'],
-            stocks_to_add = ['NOK'],
-            last_updated = "10/25/2000")
+            stocks_in_danger = get_sp_500_tickers_in_danger(),
+            stocks_to_add = get_rising_non_sp_500_tickers(),
+            last_updated = "09/26/2023 10:30:05 AM")
+
+def get_sp_500_tickers_in_danger():
+   try:
+      conn = psycopg2.connect(**db_config)
+      cursor = conn.cursor(cursor_factory=extras.RealDictCursor)
+      sql_query = "select * from tbTickerToMarketcap where SP500mmbr = true and marketcap < 10000000000 order by marketcap"
+
+      cursor.execute(sql_query)
+
+      # Fetch all the rows from the result set as dictionaries
+      rows = cursor.fetchall()
+
+      # Process the data (access columns by name)
+      return rows # Access 'sp500mmbr' column
+
+   except Exception as e:
+      print(f"Error: {e}")
+
+   finally:
+      cursor.close()
+      conn.close()
+
+def get_rising_non_sp_500_tickers():
+   try:
+      conn = psycopg2.connect(**db_config)
+      cursor = conn.cursor(cursor_factory=extras.RealDictCursor)
+      sql_query = "select * from tbTickerToMarketcap where SP500mmbr = false and marketcap > 14000000000 order by marketcap desc"
+
+      cursor.execute(sql_query)
+
+      # Fetch all the rows from the result set as dictionaries
+      rows = cursor.fetchall()
+
+      return rows
+
+   except Exception as e:
+      print(f"Error: {e}")
+
+   finally:
+      cursor.close()
+      conn.close()
 
 if __name__ == '__main__':
-   app.run()
+   db_config = {
+   "user": "woody",
+    "password": "Thereisasnakeinmyboot1!",
+    "host": "indexarb-server.postgres.database.azure.com",
+    "database": "dbSPTMI",
+    "port": 5432
+   }
+   app.run(port=8000)
